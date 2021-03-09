294a7cea7585a6f3535e5fc0a270bbb8ad50c583
Karl Marx Manzano <karlmarxmanzano@gmail.com>
2021-03-09 21:44:49 +0800

    Magic 8 ball activity

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Magic 8'),
+          backgroundColor: Colors.blue,
+        ),
+        body: MagicEight(),
+      ),
+    ),
+  );
+}
+
+class MagicEight extends StatefulWidget {
+  @override
+  _MagicEightState createState() => _MagicEightState();
+}
+
+class _MagicEightState extends State<MagicEight> {
+  int magicNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Expanded(
+          child: FlatButton(
+            onPressed: () {
+              setState(() {
+                magicNumber = Random().nextInt(4) + 1;
+              });
+            },
+            child: Image.asset('images/ball${magicNumber}.png'),
+          ),
+        ),
       ),
     );
+  }
+}
